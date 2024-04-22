681a468421a372849bd5d18d8d22dd76819e584a
m. aşkın pınar <72606403+b20724964@users.noreply.github.com>
2024-04-22 12:06:51 +0300

    add etsy best seller search

--- javascript.rs
+++ javascript.rs
@@ -12,3 +12,13 @@ let text;
     var current_url = location.host + location.pathname + location.search; 
     redirect_link = encodeURIComponent(current_url); 
     location.href='http://erank.com/analyzer?link=%27+redirect_link;})();
+
+
+javascript:(function(){
+let text;
+  let person = prompt("Aramak istediğin tagı yaz:", "derin tech");
+  if (person == null || person == "") {
+    text = "User cancelled the prompt.";
+  } else {
+    location.href='https://www.etsy.com/search?q='+ person +'&explicit=1&is_best_seller=true&ship_to=US';
+  }})();
